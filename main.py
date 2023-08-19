e0bdbdbb82fa8e4c7172ea2a57453e48b925b255
Denis Kravchenko <pochtastudenta@gmail.com>
2023-08-19 20:53:31 +0200

    first task ready

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+url = 'https://akabab.github.io/superhero-api/api/all.json'
+resp = requests.get(url)
+# newlist = sorted(resp.json(), key=lambda d: d['powerstats']['intelligence'])
+comparison_hero = ['Hulk', 'Captain America', 'Thanos']
+new_list = []
+for i in resp.json():
+    for j in comparison_hero:
+        if i['name'].lower() == j.lower():
+            new_list.append(i)
+list_sort = sorted(new_list, key=lambda d: d['powerstats']['intelligence'])
+print(f'Самый умный списка героев: {list_sort[-1]["name"]}')
