f5116ef57a947da44c783ab636a9e1022c317a98
Denis Kravchenko <pochtastudenta@gmail.com>
2023-08-24 15:23:48 +0200

    first task ready

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import requests
 
 url = 'https://akabab.github.io/superhero-api/api/all.json'
 resp = requests.get(url)
-# newlist = sorted(resp.json(), key=lambda d: d['powerstats']['intelligence'])
 comparison_hero = ['Hulk', 'Captain America', 'Thanos']
-new_list = []
-for i in resp.json():
-    for j in comparison_hero:
-        if i['name'].lower() == j.lower():
-            new_list.append(i)
-list_sort = sorted(new_list, key=lambda d: d['powerstats']['intelligence'])
-print(f'Самый умный списка героев: {list_sort[-1]["name"]}')
+
+
+def smart_hero():
+    new_list = []
+    for i in resp.json():
+        for j in comparison_hero:
+            if i['name'].lower() == j.lower():
+                new_list.append(i)
+    list_sort = sorted(new_list, key=lambda d: d['powerstats']['intelligence'])
+    print(f'Самый умный  из списка героев: {list_sort[-1]["name"]}')
+
+
+smart_hero()
